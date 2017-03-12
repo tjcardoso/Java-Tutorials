c40041ec7eab44e627310275497d1ac8eeb8775e
Todd Cardoso <toddcardoso@kosah.com>
2017-03-11 22:33:19 -0800

    Tutorial 3 strings and concat

--- Tutorial3/src/Application.java
+++ Tutorial3/src/Application.java
@@ -1,4 +1,18 @@
 
 public class Application {
-
+	public static void main(String[] args) {
+	
+		int num = 7;
+		
+		String text = "Helllo";
+		
+		String name = "Bob";
+		
+		String blank = " ";
+		
+		String message = text + ", " + name;
+		
+		System.out.println(message);
+		
+	}
 }
