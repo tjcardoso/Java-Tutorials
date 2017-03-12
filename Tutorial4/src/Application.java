fe65b268b0099ed5fc00004f24d74b102eb2c3c0
Todd Cardoso <toddcardoso@kosah.com>
2017-03-11 22:45:54 -0800

    Tutorial 4 simple for loops

--- Tutorial4/src/Application.java
+++ Tutorial4/src/Application.java
@@ -1,4 +1,13 @@
 
 public class Application {
-
+	public static void main(String[] args) {
+		boolean loop = 4 < 5;
+		
+//		System.out.println(loop);
+		
+		for(int i=0;i < 5; i++){
+			System.out.printf("The Value of i is: %d\n", i);
+		};
+	}
+	
 }
